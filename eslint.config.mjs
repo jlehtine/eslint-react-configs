8cbba3bbf291b25aac07dcb2a26bf7153be70e47
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2025-03-16 02:07:06 +0200

    Prettier print width of 120 chars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,4 @@
 import js from "@eslint/js";
 import globals from "globals";
 
-export default [
-  { languageOptions: { globals: globals.browser } },
-  js.configs.recommended,
-];
+export default [{ languageOptions: { globals: globals.browser } }, js.configs.recommended];
